3acbc89607aa98744654e7179b37152eabe9edf5
Dark5ync <110257029+Dark5ync@users.noreply.github.com>
2022-07-30 17:33:46 +0300

    Delete assets directory

--- assets/js/preloader.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	$('#preloader-dots').delay(350).fadeOut(300,function() {
-		$('#preloader').animate({opacity: '0'},300,function() {
-			$('#preloader').fadeOut(10);
-		});
-	});
-
-});
